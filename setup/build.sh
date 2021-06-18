641ee4b718163b00119c95f5d6a5c4e44a27c716
Sahil Mishra <sahilmishra0012@outlook.com>
2021-06-18 23:15:35 +0530

    Deleted Redundant Shell Scripts

--- setup/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# To be executed from project root
-
-# Mock http server
-docker build -t iudx/adaptor-mockserver:latest -f setup/mockserver/mockserver.dockerfile .
-docker build -t iudx/adaptor-server:latest -f setup/server/server.dockerfile .
