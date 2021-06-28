ae96a9d1dcea71a17cb9f65d549816523078bcf3
Sahil Mishra <sahilmishra0012@outlook.com>
2021-06-28 19:07:36 +0530

    Changes Pushed

--- flink-connectors/FlinkStreamingKuduRemoteRMQ/src/main/java/RMQKuduSink.java
+++ flink-connectors/FlinkStreamingKuduRemoteRMQ/src/main/java/RMQKuduSink.java
@@ -25,15 +25,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.TimeZone;
+import java.util.*;
 
 
 public class RMQKuduSink {
 
-    private static final String KUDU_MASTERS = System.getProperty("kuduMasters", "127.0.0.1:7051");
+    private static final String KUDU_MASTERS = System.getProperty("kuduMasters", "kudu_kudu-master-1_1:7051");
 
     public static void main(String[] args) throws Exception {
 
@@ -62,7 +59,7 @@ public class RMQKuduSink {
 
 
 
-        String tableName = "surat_data89";
+        String tableName = "surat_itms_data_june2";
         KuduClient client = new KuduClient.KuduClientBuilder(KUDU_MASTERS).build();
 
         try {
@@ -72,16 +69,15 @@ public class RMQKuduSink {
             System.out.println("Table already exist or has creation problem.");
         }
 
-
         // kudu sink
-        KuduWriterConfig writerConfig = KuduWriterConfig.Builder.setMasters("127.0.0.1:7051").build();
+        KuduWriterConfig writerConfig = KuduWriterConfig.Builder.setMasters("kudu_kudu-master-1_1:7051").build();
 
         KuduSink<Row> sink = new KuduSink<>(
                 writerConfig,
                 KuduTableInfo.forTable(tableName),
                 new RowOperationMapper(
-                        new String[]{"trip_id","id","route_id","trip_direction","last_stop_arrival_time","vehicle_label","license_plate","last_stop_id","speed","trip_delay","location_type","coordinate0","coordinate1"},
-                        AbstractSingleOperationMapper.KuduOperation.UPSERT));
+                        new String[]{"primary_key","trip_id","id","route_id","trip_direction","actual_trip_start_time","last_stop_arrival_time","vehicle_label","license_plate","last_stop_id","speed","observationDateTime","trip_delay","location_type","coordinate0","coordinate1"},
+                        AbstractSingleOperationMapper.KuduOperation.INSERT));
 
         stream.addSink(sink);
 
@@ -91,9 +87,12 @@ public class RMQKuduSink {
 
     static void createExampleTable(KuduClient client, String tableName) throws KuduException {
 
-        List<ColumnSchema> columns = new ArrayList<>(13);
-        columns.add(new ColumnSchema.ColumnSchemaBuilder("trip_id", Type.STRING)
+        List<ColumnSchema> columns = new ArrayList<>(16);
+        columns.add(new ColumnSchema.ColumnSchemaBuilder("primary_key", Type.STRING)
                 .key(true)
+                .build());
+        columns.add(new ColumnSchema.ColumnSchemaBuilder("trip_id", Type.STRING)
+                .nullable(true)
                 .build());
         columns.add(new ColumnSchema.ColumnSchemaBuilder("id", Type.STRING).nullable(true)
                 .build());
@@ -101,8 +100,8 @@ public class RMQKuduSink {
                 .build());
         columns.add(new ColumnSchema.ColumnSchemaBuilder("trip_direction", Type.STRING).nullable(true)
                 .build());
-//        columns.add(new ColumnSchema.ColumnSchemaBuilder("actual_trip_start_time", Type.UNIXTIME_MICROS).nullable(true)
-//                .build());
+        columns.add(new ColumnSchema.ColumnSchemaBuilder("actual_trip_start_time", Type.UNIXTIME_MICROS).nullable(true)
+                .build());
         columns.add(new ColumnSchema.ColumnSchemaBuilder("last_stop_arrival_time", Type.STRING).nullable(true)
                 .build());
         columns.add(new ColumnSchema.ColumnSchemaBuilder("vehicle_label", Type.STRING).nullable(true)
@@ -113,8 +112,8 @@ public class RMQKuduSink {
                 .build());
         columns.add(new ColumnSchema.ColumnSchemaBuilder("speed", Type.DOUBLE).nullable(true)
                 .build());
-//        columns.add(new ColumnSchema.ColumnSchemaBuilder("observationDateTime", Type.UNIXTIME_MICROS).nullable(true)
-//                .build());
+        columns.add(new ColumnSchema.ColumnSchemaBuilder("observationDateTime", Type.UNIXTIME_MICROS).nullable(true)
+                .build());
         columns.add(new ColumnSchema.ColumnSchemaBuilder("trip_delay", Type.STRING).nullable(true)
                 .build());
         columns.add(new ColumnSchema.ColumnSchemaBuilder("location_type", Type.STRING).nullable(true)
@@ -128,7 +127,7 @@ public class RMQKuduSink {
         CreateTableOptions cto = new CreateTableOptions();
         cto.setNumReplicas(1);
         List<String> hashKeys = new ArrayList<>(1);
-        hashKeys.add("trip_id");
+        hashKeys.add("primary_key");
         int numBuckets = 8;
         cto.addHashPartitions(hashKeys, numBuckets);
 
@@ -139,48 +138,50 @@ public class RMQKuduSink {
 
 
     static class CustomDeserializationSchema extends AbstractDeserializationSchema<Row> {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
         @Override
         public Row deserialize(byte[] bytes) {
 
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
             df.setTimeZone(TimeZone.getTimeZone("UTC"));
 
-            String base64Encoded = DatatypeConverter.printBase64Binary(bytes);
 
-            byte[] base64Decoded = DatatypeConverter.parseBase64Binary(base64Encoded);
-            String streamData = new String(base64Decoded);
+            String streamData = new String(bytes);
 
             JSONObject obj = new JSONObject(streamData);
             JSONObject location = obj.getJSONObject("location");
             JSONArray coordinates = location.getJSONArray("coordinates");
-//            String actualTripStartTime =  obj.get("actual_trip_start_time").toString();
-//            String observationDateTime =  obj.get("observationDateTime").toString();
+            String actualTripStartTime =  obj.get("actual_trip_start_time").toString();
+            String observationDateTime =  obj.get("observationDateTime").toString();
 
-            Row values = new Row(13);
-            values.setField(0, obj.get("trip_id"));
-            values.setField(1, obj.get("id"));
-            values.setField(2, obj.get("route_id"));
-            values.setField(3, obj.get("trip_direction"));
-//            try {
-//                values.setField(4, df.parse(actualTripStartTime).getTime()*1000);
-//            } catch (ParseException e) {
-//                System.out.println("Error"+e);
-//            }
-            values.setField(4, obj.get("last_stop_arrival_time").toString());
-            values.setField(5, obj.get("vehicle_label"));
-            values.setField(6, obj.get("license_plate"));
-            values.setField(7, obj.get("last_stop_id"));
-            values.setField(8, obj.get("speed"));
-//            try {
-//                values.setField(10, df.parse(observationDateTime).getTime()*1000);
-//            } catch (ParseException e) {
-//                System.out.println("Error"+e);
-//            }
-            values.setField(9, obj.get("trip_delay").toString());
-            values.setField(10,location.get("type"));
-            values.setField(11,coordinates.get(0));
-            values.setField(12,coordinates.get(1));
+            String keyString=obj.get("trip_id")+obj.get("observationDateTime").toString()+ UUID.randomUUID();
+            String primaryKey = UUID.nameUUIDFromBytes(keyString.getBytes()).toString();
+
+            Row values = new Row(16);
+            values.setField(0, primaryKey);
+            values.setField(1, obj.get("trip_id"));
+            values.setField(2, obj.get("id"));
+            values.setField(3, obj.get("route_id"));
+            values.setField(4, obj.get("trip_direction"));
+            try {
+                values.setField(5, df.parse(actualTripStartTime).getTime()*1000);
+            } catch (ParseException e) {
+                System.out.println("Error"+e);
+            }
+            values.setField(6, obj.get("last_stop_arrival_time").toString());
+            values.setField(7, obj.get("vehicle_label"));
+            values.setField(8, obj.get("license_plate"));
+            values.setField(9, obj.get("last_stop_id"));
+            values.setField(10, obj.get("speed"));
+            try {
+                values.setField(11, df.parse(observationDateTime).getTime()*1000);
+            } catch (ParseException e) {
+                System.out.println("Error"+e);
+            }
+            values.setField(12, obj.get("trip_delay").toString());
+            values.setField(13,location.get("type"));
+            values.setField(14,coordinates.get(0));
+            values.setField(15,coordinates.get(1));
 
             return values;
         }
